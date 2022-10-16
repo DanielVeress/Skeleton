537d2a9cee87c34c946460837e40c42e95cad1fa
DanielVeress <daniel.veress3@gmail.com>
2022-10-16 14:09:12 +0200

    Pontok lerakhatóak

--- Skeleton/Skeleton.cpp
+++ Skeleton/Skeleton.cpp
@@ -59,30 +59,192 @@ const char * const fragmentSource = R"(
 	}
 )";
 
+// 2D camera
+struct Camera {
+	float wCx, wCy;	// center in world coordinates
+	float wWx, wWy;	// width and height in world coordinates
+public:
+	Camera() {
+		wCx = 0;
+		wCy = 0;
+		wWx = 20;
+		wWy = 20;
+	}
+
+	mat4 V() { // view matrix: translates the center to the origin
+		return mat4(1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					-wCx, -wCy, 0, 1);
+	}
+
+	mat4 P() { // projection matrix: scales it to be a square of edge length 2
+		return mat4(2 / wWx, 0, 0, 0,
+					0, 2 / wWy, 0, 0,
+					0, 0, 1, 0,
+					0, 0, 0, 1);
+	}
+
+	mat4 Vinv() { // inverse view matrix
+		return mat4(1, 0, 0, 0,
+					0, 1, 0, 0,
+					0, 0, 1, 0,
+					wCx, wCy, 0, 1);
+	}
+
+	mat4 Pinv() { // inverse projection matrix
+		return mat4(wWx / 2, 0, 0, 0,
+					0, wWy / 2, 0, 0,
+					0, 0, 1, 0,
+					0, 0, 0, 1);
+	}
+};
+
+Camera camera;	// 2D camera
 GPUProgram gpuProgram; // vertex and fragment shaders
-unsigned int vao;	   // virtual world on the GPU
+const int nTesselatedVertices = 100;
+
+class Curve {
+	unsigned int vaoCurve, vboCurve;
+	unsigned int vaoCtrlPoints, vboCtrlPoints;
+	unsigned int vaoConvexHull, vboConvexHull;
+
+	float B(int i, float t) {
+		int n = wCtrlPoints.size() - 1; // n deg polynomial = n+1 pts!
+		float choose = 1;
+		for (int j = 1; j <= i; j++) choose *= (float)(n - j + 1) / j;
+		return choose * pow(t, i) * pow(1 - t, n - i);
+	}
+protected:
+	std::vector<vec4> wCtrlPoints;		// coordinates of control points
+	std::vector<vec4> ts;				// parameter (knots) values
+public:
+	Curve() {
+		// Curve
+		glGenVertexArrays(1, &vaoCurve);
+		glBindVertexArray(vaoCurve);
+
+		glGenBuffers(1, &vboCurve); // Generate 1 vertex buffer object
+		glBindBuffer(GL_ARRAY_BUFFER, vboCurve);
+		// Enable the vertex attribute arrays
+		glEnableVertexAttribArray(0);  // attribute array 0
+		// Map attribute array 0 to the vertex data of the interleaved vbo
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL); // attribute array, components/attribute, component type, normalize?, stride, offset
+
+		// Control Points
+		glGenVertexArrays(1, &vaoCtrlPoints);
+		glBindVertexArray(vaoCtrlPoints);
+
+		glGenBuffers(1, &vboCtrlPoints); // Generate 1 vertex buffer object
+		glBindBuffer(GL_ARRAY_BUFFER, vboCtrlPoints);
+		// Enable the vertex attribute arrays
+		glEnableVertexAttribArray(0);  // attribute array 0
+		// Map attribute array 0 to the vertex data of the interleaved vbo
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), NULL); // attribute array, components/attribute, component type, normalize?, stride, offset
+
+		// Convex Hull
+		glGenVertexArrays(1, &vaoConvexHull);
+		glBindVertexArray(vaoConvexHull);
+
+		glGenBuffers(1, &vboConvexHull); // Generate 1 vertex buffer object
+		glBindBuffer(GL_ARRAY_BUFFER, vboConvexHull);
+		// Enable the vertex attribute arrays
+		glEnableVertexAttribArray(0);  // attribute array 0
+		// Map attribute array 0 to the vertex data of the interleaved vbo
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), NULL); // attribute array, components/attribute, component type, normalize?, stride, offset
+	}
+
+	virtual vec4 r(float t) {
+		vec4 wPoint = vec4(0, 0, 0, 0);
+		for (unsigned int n = 0; n < wCtrlPoints.size(); n++) wPoint += wCtrlPoints[n] * B(n, t);
+		return wPoint;
+	}
+
+	virtual float tStart() { return 0; }
+	virtual float tEnd() { return 1; }
+
+	virtual void AddControlPoint(float cX, float cY) {
+		vec4 wVertex = vec4(cX, cY, 0, 1) * camera.Pinv() * camera.Vinv();
+		wCtrlPoints.push_back(wVertex);
+	}
+
+	// Returns the selected control point or -1
+	int PickControlPoint(float cX, float cY) {
+		vec4 wVertex = vec4(cX, cY, 0, 1) * camera.Pinv() * camera.Vinv();
+		for (unsigned int p = 0; p < wCtrlPoints.size(); p++) {
+			if (dot(wCtrlPoints[p] - wVertex, wCtrlPoints[p] - wVertex) < 0.1) return p;
+		}
+		return -1;
+	}
+
+	void MoveControlPoint(int p, float cX, float cY) {
+		vec4 wVertex = vec4(cX, cY, 0, 1) * camera.Pinv() * camera.Vinv();
+		wCtrlPoints[p] = wVertex;
+	}
+
+	void Draw() {
+		mat4 VPTransform = camera.V() * camera.P();
+
+		//VPTransform.SetUniform(gpuProgram.getId(), "MVP");
+		gpuProgram.setUniform(VPTransform, "MVP");
+
+		int colorLocation = glGetUniformLocation(gpuProgram.getId(), "color");
+
+		if (wCtrlPoints.size() > 2) {	// draw convex hull
+			std::vector<vec4> triangleVertex;
+			for (unsigned int i = 0; i < wCtrlPoints.size(); i++) {
+				for (unsigned int j = i + 1; j < wCtrlPoints.size(); j++) {
+					for (unsigned int k = j + 1; k < wCtrlPoints.size(); k++) {
+						triangleVertex.push_back(wCtrlPoints[i]);
+						triangleVertex.push_back(wCtrlPoints[j]);
+						triangleVertex.push_back(wCtrlPoints[k]);
+					}
+				}
+			}
+			glBindVertexArray(vaoConvexHull);
+			glBindBuffer(GL_ARRAY_BUFFER, vboConvexHull);
+			glBufferData(GL_ARRAY_BUFFER, triangleVertex.size() * 4 * sizeof(float), &triangleVertex[0], GL_DYNAMIC_DRAW);
+			if (colorLocation >= 0) glUniform3f(colorLocation, 0, 0.5f, 0.5f);
+			glDrawArrays(GL_TRIANGLES, 0, triangleVertex.size());
+		}
+
+		if (wCtrlPoints.size() > 0) {	// draw control points
+			glBindVertexArray(vaoCtrlPoints);
+			glBindBuffer(GL_ARRAY_BUFFER, vboCtrlPoints);
+			glBufferData(GL_ARRAY_BUFFER, wCtrlPoints.size() * 4 * sizeof(float), &wCtrlPoints[0], GL_DYNAMIC_DRAW);
+			if (colorLocation >= 0) glUniform3f(colorLocation, 1, 0, 0);
+			glPointSize(10.0f);
+			glDrawArrays(GL_POINTS, 0, wCtrlPoints.size());
+		}
+
+		if (wCtrlPoints.size() >= 2) {	// draw curve
+			std::vector<float> vertexData;
+			for (int i = 0; i < nTesselatedVertices; i++) {	// Tessellate
+				float tNormalized = (float)i / (nTesselatedVertices - 1);
+				float t = tStart() + (tEnd() - tStart()) * tNormalized;
+				vec4 wVertex = r(t);
+				vertexData.push_back(wVertex.x);
+				vertexData.push_back(wVertex.y);
+			}
+			// copy data to the GPU
+			glBindVertexArray(vaoCurve);
+			glBindBuffer(GL_ARRAY_BUFFER, vboCurve);
+			glBufferData(GL_ARRAY_BUFFER, vertexData.size() * sizeof(float), &vertexData[0], GL_DYNAMIC_DRAW);
+			if (colorLocation >= 0) glUniform3f(colorLocation, 1, 1, 0);
+			glDrawArrays(GL_LINE_STRIP, 0, nTesselatedVertices);
+		}
+	}
+};
+
+// The virtual world: collection of two objects
+Curve* curve;
 
 // Initialization, create an OpenGL context
 void onInitialization() {
 	glViewport(0, 0, windowWidth, windowHeight);
+	glLineWidth(2.0f);
 
-	glGenVertexArrays(1, &vao);	// get 1 vao id
-	glBindVertexArray(vao);		// make it active
-
-	unsigned int vbo;		// vertex buffer object
-	glGenBuffers(1, &vbo);	// Generate 1 buffer
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// Geometry with 24 bytes (6 floats or 3 x 2 coordinates)
-	float vertices[] = { -0.8f, -0.8f, -0.6f, 1.0f, 0.8f, -0.2f };
-	glBufferData(GL_ARRAY_BUFFER, 	// Copy to GPU target
-		sizeof(vertices),  // # bytes
-		vertices,	      	// address
-		GL_STATIC_DRAW);	// we do not change later
-
-	glEnableVertexAttribArray(0);  // AttribArray 0
-	glVertexAttribPointer(0,       // vbo -> AttribArray 0
-		2, GL_FLOAT, GL_FALSE, // two floats/attrib, not fixed-point
-		0, NULL); 		     // stride, offset: tightly packed
+	curve = new Curve();
 
 	// create program for the GPU
 	gpuProgram.create(vertexSource, fragmentSource, "outColor");
@@ -93,20 +255,7 @@ void onDisplay() {
 	glClearColor(0, 0, 0, 0);     // background color
 	glClear(GL_COLOR_BUFFER_BIT); // clear frame buffer
 
-	// Set color to (0, 1, 0) = green
-	int location = glGetUniformLocation(gpuProgram.getId(), "color");
-	glUniform3f(location, 0.0f, 1.0f, 0.0f); // 3 floats
-
-	float MVPtransf[4][4] = { 1, 0, 0, 0,    // MVP matrix, 
-							  0, 1, 0, 0,    // row-major!
-							  0, 0, 1, 0,
-							  0, 0, 0, 1 };
-
-	location = glGetUniformLocation(gpuProgram.getId(), "MVP");	// Get the GPU location of uniform variable MVP
-	glUniformMatrix4fv(location, 1, GL_TRUE, &MVPtransf[0][0]);	// Load a 4x4 row-major float matrix to the specified location
-
-	glBindVertexArray(vao);  // Draw call
-	glDrawArrays(GL_TRIANGLES, 0 /*startIdx*/, 3 /*# Elements*/);
+	curve->Draw();
 
 	glutSwapBuffers(); // exchange buffers for double buffering
 }
@@ -118,33 +267,34 @@ void onKeyboard(unsigned char key, int pX, int pY) {
 
 // Key of ASCII code released
 void onKeyboardUp(unsigned char key, int pX, int pY) {
+
 }
 
+int pickedControlPoint = -1;
+
 // Move mouse with key pressed
-void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
+void onMouseMotion(int pX, int pY) {
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
+	if (pickedControlPoint >= 0) curve->MoveControlPoint(pickedControlPoint, cX, cY);
 }
 
 // Mouse click event
-void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
+void onMouse(int button, int state, int pX, int pY) {
 	// Convert to normalized device space
 	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
 	float cY = 1.0f - 2.0f * pY / windowHeight;
 
-	char * buttonStat;
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)		// GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON and GLUT_DOWN / GLUT_UP
+		curve->AddControlPoint(cX, cY);
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN)		// GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON and GLUT_DOWN / GLUT_UP
+		pickedControlPoint = curve->PickControlPoint(cX, cY);
+	if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP) {  // GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON and GLUT_DOWN / GLUT_UP
+		pickedControlPoint = -1;
+		return;
 	}
-
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
+	
+	glutPostRedisplay();     // redraw
 }
 
 // Idle event indicating that some time elapsed: do animation here
